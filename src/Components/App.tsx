db614f5192f853ed93aa4f11ba3c38cef2d8314c
sweetmilkys <sweetmilky.dev@gmail.com>
2019-08-08 23:52:13 +0900

    Container Presenter Pattern으로 변경

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Main from "../Routes/Main";
 
 const App: React.FC = () => {
-  return <div className="App">wanted</div>;
+  return <Main />;
 };
 
 export default App;
