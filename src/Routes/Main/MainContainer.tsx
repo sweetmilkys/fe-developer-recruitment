20b2598280082f9750b48ddbb4a0d0eaeea7766e
sweetmilkys <sweetmilky.dev@gmail.com>
2019-08-10 23:12:44 +0900

    에러 인디케이터 추가

--- src/Routes/Main/MainContainer.tsx
+++ src/Routes/Main/MainContainer.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState, useRef } from "react";
 import MainPresenter from "./MainPresenter";
 import axios from "axios";
 import Loader from "../../Components/Loader";
+import Error from "../../Components/Error";
 
 const MainContainer: React.FC = () => {
   const [loading, setLodaing] = useState(true);
+  const [error, setError] = useState(false);
   const [filters, setFilters] = useState();
   const [sort, setSort] = useState({
     selected: true,
@@ -47,10 +49,10 @@ const MainContainer: React.FC = () => {
         setJobs(jobsData);
         links.current = links;
       } catch (error) {
-        console.log(error);
-        // 에러세팅
+        // 에러화면 보이도록 세팅
+        setError(true);
       } finally {
-        // 로딩화면에서 전환
+        // 로딩화면에서 전환되도록 세팅
         setLodaing(false);
       }
     };
@@ -59,6 +61,8 @@ const MainContainer: React.FC = () => {
 
   return loading ? (
     <Loader />
+  ) : error ? (
+    <Error />
   ) : (
     <MainPresenter
       sort={sort}
